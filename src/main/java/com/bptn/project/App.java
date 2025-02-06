68e287a2e300799dad6bd3a92026fcdb34b89dd2
abimbola-ai <ojikutu.abimbola@gmail.com>
2025-02-05 22:32:52 -0500

    added color features to the menu

--- src/main/java/com/bptn/project/App.java
+++ src/main/java/com/bptn/project/App.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 import com.bptn.project.services.ExpenseService;
 
 public class App {
+	// Define color codes
+	public static final String RESET = "\u001B[0m";
+	public static final String RED = "\u001B[31m";
+	public static final String GREEN = "\u001B[32m";
+	
 	public static void main(String[] args) {
 		// Create an instance of the Expense service
 		ExpenseService expenseService = new ExpenseService();
@@ -13,13 +18,14 @@ public class App {
 		Scanner scanner = new Scanner(System.in);
 		
 	
-			System.out.println("Welcome to the expense tracker program.");
-			System.out.println("Main Menu. Please choose among the following options: ");
-			System.out.println("1: Add a new expense");
-			System.out.println("2: View your expenses");
-			System.out.println("3: Edit an expense");
-			System.out.println("3: Delete new expense");
-			System.out.println("5: Exit Main Menu");
+			System.out.println("\n" + GREEN + "Welcome to the expense tracker program." + RESET);
+			System.out.println(RED + "Main Menu. Please choose among the following options: " + RESET);
+			System.out.println("");
+			System.out.println(RED + "1:" + RESET + GREEN + " Add a new expense" + RESET);
+			System.out.println(RED + "2:" + RESET + GREEN +" View your expenses");
+			System.out.println(RED + "3:" + RESET + GREEN +" Edit an expense");
+			System.out.println(RED + "4:" + RESET + GREEN +" Delete new expense");
+			System.out.println(RED + "5:" + RESET + GREEN +" Exit Main Menu");
 			
 			int selection = scanner.nextInt();
 			
